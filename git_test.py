22bcf5ee7e3a84be8a50914ca992b0b2d2bef121
Sosa09 <soufiane.arrazouki@hotmail.com>
2023-01-26 20:46:48 +0100

    Soufiane

--- git_test.py
+++ git_test.py
@@ -1,9 +1,3 @@
 print("eerste lijn")
-<<<<<<< HEAD
 print("nintendo ds")
 print("Hello world")
-=======
-print("dsds")
-print("Hello world")
-print("Yo my nigga")
->>>>>>> 936ea10403f1e6f9cc6eb8e2f6809abe2801d1ce
